62c73f4c9762e55bb87e925676b32b650a71f8fd
Louis Hough <sirlouishough@gmail.com>
2020-01-19 00:56:00 +0000

    operate function

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 function add(a,b){
-    return a + b;
+return a + b;
 }
 
 function sub(a,b){
-    return a - b;
+return a - b;
 }
 
 function multiply(a,b){
-    return a * b;
+return a * b;
 }
 
 function divide(a,b){
-    return a / b;
+return a / b;
+}
+
+function operate(num1, num2, operation){
+let result;
+switch (operation) {
+    case "add": 
+    result = add(num1, num2);
+    break;
+    case "subtract":
+    result = sub(num1, num2);
+    break;
+    case "multiply":
+    result = multiply(num1, num2);
+    break;
+    case "divide":
+    result = divide(num1, num2);
+    break;
+    }
+return result;
 }
